adfc1b2902d74fa6897e8a67c63ffb999ea95656
Max Kaye <m@xk.io>
2015-05-06 07:57:20 +1000

    Explorer is semi-functional!

--- nvbtally/explorer.py
+++ nvbtally/explorer.py
@@ -1,12 +1,42 @@
+from pycoin.encoding import b2a_base58
+
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
 
-from nvbtally.models import engine
+from sqlalchemy.orm import sessionmaker
+
+from .models import engine, NetworkSettings, Resolution, Vote, ValidVoter
+
+Session = sessionmaker(bind=engine)
+
+
+def _all_b_to_str(l):
+    return list(map(lambda i: str(i), l))
 
 
 def index_view(request):
     return {}
+
+
+def info_json(request):
+    session = Session()
+    ns = session.query(NetworkSettings).first()
+    return {
+        'Admin ID': str(ns.admin_address),
+        'Network Name': str(ns.network_name),
+    }
+
+
+def resolutions_json(request):
+    session = Session()
+    rs = session.query(Resolution).all()
+    rs_unresolved = list(map(lambda r: [str(r.res_name), str(r.url), r.end_timestamp], filter(lambda r: r.resolved == 0, rs)))
+    rs_resolved = list(map(lambda r: [str(r.res_name), str(r.url), r.votes_for // 255, r.votes_total // 255], filter(lambda r: r.resolved == 1, rs)))
+    return {
+        'unfinalized': rs_unresolved,
+        'finalized': rs_resolved,
+    }
 
 
 def main(global_config, **settings):
@@ -15,6 +45,12 @@ def main(global_config, **settings):
 
     config.add_route('index', '/')
     config.add_view(index_view, route_name='index', renderer='index.pt')
+    config.add_route('info', '/info')
+    config.add_view(info_json, route_name='info', renderer='json')
+
+    config.add_route('resolutions', '/resolutions')
+    config.add_view(resolutions_json, route_name='resolutions', renderer='json')
+
 
     config.add_static_view(name='static', path='nvbtally:static')
 
--- nvbtally/static/nvbtally.js
+++ nvbtally/static/nvbtally.js
@@ -4,7 +4,7 @@
     app.controller('TabController', ['$log', function($log){
         var tab = this;
         tab.tabsLeft = ['info', 'resolutions', 'votes'];
-        tab.tabsRight = ['about'];
+        tab.tabsRight = ['voters', 'about'];
 
         tab.set = function(t){
             tab.current = t;
@@ -20,7 +20,29 @@
     app.controller('InfoCtrl', ['$log', '$http', function($log, $http){
         var info = this;
 
+        info.content = {};
 
+        $http.get('/info')
+            .success(function(data){
+                info.content = data;
+                $log.log(data);
+            }).error(function(error,and,other,things){
+                $log.log(error);
+            });
+    }]);
+
+    app.controller('ResolutionCtrl', ['$log', '$http', function($log, $http){
+        var res = this;
+
+        res.unfinalized = [];
+        res.finalized = [];
+
+        $http.get('/resolutions')
+            .success(function(data){
+                res.unfinalized = data.unfinalized;
+                res.finalized = data.finalized;
+            }).error(function(error,and,other,things){
+            });
     }]);
 
 })();
